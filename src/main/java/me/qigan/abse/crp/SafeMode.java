9b1d84a3e59e2c0baeda92b8e09f95388677d2b9
Sirtage <zirocilt@gmail.com>
2023-12-28 02:58:18 +0300

    And again some typos

--- src/main/java/me/qigan/abse/crp/SafeMode.java
+++ src/main/java/me/qigan/abse/crp/SafeMode.java
@@ -4,11 +4,16 @@ package me.qigan.abse.crp;
 public class SafeMode extends Module{
     @Override
     public String id() {
-        return "safe_mod";
+        return "safe_mode";
     }
 
     @Override
     public String description() {
         return "If enabled - preserve you from the most of possible crashes(caused by mode). Lower performance, can cause incorrect work of module";
+    }
+
+    @Override
+    public String fname() {
+        return "Safe mode";
     }
 }
