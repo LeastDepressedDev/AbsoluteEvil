356dac3a0f3e071a0f541bb5b50fadbb4d709e39
Sirtage <zirocilt@gmail.com>
2024-03-28 01:13:58 +0300

    Fixed museum route

--- src/main/java/me/qigan/abse/mapping/rooms/r2x2/RoomMuseum.java
+++ src/main/java/me/qigan/abse/mapping/rooms/r2x2/RoomMuseum.java
@@ -65,8 +65,8 @@ public class RoomMuseum extends RoomTemplate {
                 new BBox(-27, 70, 30, -28, 70, 32, Blocks.air),
                 new BBox(22, 89, 29, 22, 88, 33, Blocks.air),
                 new BBox(17, 81, 35, 17, 81, 35, Blocks.air),
-                new BBox(22, 69, 38, 23, 69, 38, Blocks.stained_glass),
-                new BBox(23, 69, 39, 23, 69, 39, Blocks.stained_glass),
+                new BBox(22, 68, 38, 23, 68, 38, Blocks.stained_glass),
+                new BBox(23, 68, 39, 23, 68, 39, Blocks.stained_glass),
                 new BBox(22, 68, 39, 22, 65, 39, Blocks.air),
                 new BBox(22, 66, 39, 22, 65, 41, Blocks.air)
         );
