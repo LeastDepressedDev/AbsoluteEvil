31b59d32fc457d85c37e1d6d114744f71ac23c12
Sirtage <zirocilt@gmail.com>
2024-06-10 23:08:11 +0300

    Updated version(shhhh)

--- src/main/java/me/qigan/abse/Index.java
+++ src/main/java/me/qigan/abse/Index.java
@@ -25,7 +25,7 @@ public class Index
     public static PositionConfig POS_CFG;
 
     public static final String MODID = "abse";
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "2.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
